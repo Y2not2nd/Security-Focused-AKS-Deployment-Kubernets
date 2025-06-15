8e8e3056b9bd978e7340db140cc60bde1f7224af
yassin <yassin_123@hotmail.co.uk>
2025-06-15 20:47:03 +0100

    v3

--- src/backend/main.go
+++ src/backend/main.go
@@ -2,12 +2,13 @@ package main
 
 import (
     "context"
+    "encoding/json"
     "fmt"
     "log"
     "net/http"
     "os"
     "time"
-    "encoding/json"
+
     "go.mongodb.org/mongo-driver/bson"
     "go.mongodb.org/mongo-driver/mongo"
     "go.mongodb.org/mongo-driver/mongo/options"
@@ -21,12 +22,11 @@ type Counter struct {
 }
 
 func main() {
-    // Read MongoDB connection string from Vault-injected file
-    uriBytes, err := os.ReadFile("/vault/secrets/db-creds.txt")
-    if err != nil {
-        log.Fatalf("Failed to read Mongo URI from file: %v", err)
+    // Read MongoDB URI from environment variable
+    uri := os.Getenv("MONGO_URI")
+    if uri == "" {
+        log.Fatal("MONGO_URI environment variable not set")
     }
-    uri := string(uriBytes)
 
     // Connect to MongoDB
     ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
@@ -39,8 +39,8 @@ func main() {
         log.Fatalf("Mongo ping error: %v", err)
     }
 
-    db := client.Database("mydb") // Database name as per connection URI
-    collection = db.Collection("visits") // Collection to store visit count
+    db := client.Database("mydb") // Database name as per URI
+    collection = db.Collection("visits")
 
     http.HandleFunc("/api/ping", pingHandler)
     log.Println("Backend server is running on port 8080...")
@@ -56,7 +56,6 @@ func pingHandler(w http.ResponseWriter, r *http.Request) {
     ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
     defer cancel()
 
-    // Atomically increment the counter document in MongoDB
     opts := options.FindOneAndUpdate().SetUpsert(true).SetReturnDocument(options.After)
     result := collection.FindOneAndUpdate(ctx,
         bson.M{"_id": "counter"},
@@ -77,4 +76,4 @@ func pingHandler(w http.ResponseWriter, r *http.Request) {
     msg := fmt.Sprintf("Hello, you are visitor #%d", counter.Count)
     w.Header().Set("Content-Type", "application/json")
     json.NewEncoder(w).Encode(map[string]string{"message": msg})
-} 
+}
